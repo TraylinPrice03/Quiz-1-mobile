8791147530207b90aa5244af4585b5c9fe859a71
TraylinPrice03 <156850386+TraylinPrice03@users.noreply.github.com>
2025-11-08 22:21:19 -0500

    UserAdapter.java

---
+++ UserAdapter.java
@@ -0,0 +1,57 @@
+package com.example.userapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+    private Context context;
+    private List<User> userList;
+
+    public UserAdapter(Context context, List<User> userList) {
+        this.context = context;
+        this.userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        User user = userList.get(position);
+        holder.tvName.setText(user.name);
+        holder.tvEmail.setText(user.email);
+        holder.tvPhone.setText(user.phone);
+
+        holder.itemView.setOnClickListener(v ->
+                Toast.makeText(context, "You selected " + user.name, Toast.LENGTH_SHORT).show()
+        );
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName, tvEmail, tvPhone;
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvEmail = itemView.findViewById(R.id.tvEmail);
+            tvPhone = itemView.findViewById(R.id.tvPhone);
+        }
+    }
+}
