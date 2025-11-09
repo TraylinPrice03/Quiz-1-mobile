51160c0a7252fa217ecd5fe60ba239d9fa8c2653
TraylinPrice03 <156850386+TraylinPrice03@users.noreply.github.com>
2025-11-08 22:19:47 -0500

    User.java

---
+++ User.java
@@ -0,0 +1,11 @@
+package com.example.userapp;
+
+public class User {
+    String name, email, phone;
+
+    public User(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
